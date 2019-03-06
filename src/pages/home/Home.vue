96e64ca6f4de322ef47931aec3794728fa2d2efb
jiaxiaowei <jiaxiaowei_unity@hotmail.com>
2019-03-06 20:33:58 +0800

    add header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
-        hello world
+        <HomeHeader/>
     </div>
 </template>
 
 <script>
+    import HomeHeader from './components/Header'
     export default {
-        name: "Home"
+        name: "Home",
+        components: {HomeHeader}
     }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import Vue from 'vue/dist/vue.esm.js'
+import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home.vue'
 Vue.use(Router);
